61bde9ef6caa20b9cd4a3b1d1b26332a3f1da8de
nathanhmiles <60706094+nathanhmiles@users.noreply.github.com>
2021-01-12 12:59:18 +0000

    Removed dragdropcontext from cardrow

--- src/components/layout/CardRow.js
+++ src/components/layout/CardRow.js
@@ -1,33 +1,13 @@
 import React, { useState } from 'react';
-<<<<<<< Updated upstream
 import {Droppable, Draggable} from 'react-beautiful-dnd';
 import SynergyCounter from './SynergyCounter';
 import Card from 'components/cards/Card';
 import CounterArea from 'components/layout/CounterArea';
 import data from 'data';
-=======
-import SynergyCounter from './SynergyCounter';
-import Card from 'components/cards/Card';
-import CounterArea from 'components/layout/CounterArea';
-import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
->>>>>>> Stashed changes
 
 export default function CardRow(props) {
   const [rowCards, setRowCards] = useState([]);
   const synergy = 0;
-<<<<<<< Updated upstream
-=======
-  const [rowCards, setRowCards] = useState([]);
->>>>>>> Stashed changes
-  
-  function handleOnDragEnd(result) {
-    if(!result.destination) return;
-    const items = Array.from(rowCards);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setRowCards(items);
-  }
 
   return (
     <div className="rowarea">
