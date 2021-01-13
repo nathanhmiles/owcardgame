f50c334154c6df7261c27a4fdd226fd4d7241cee
nathanhmiles <60706094+nathanhmiles@users.noreply.github.com>
2021-01-13 18:00:23 +0000

    Add comments and clean up code

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,23 +1,27 @@
-import React, { useContext } from 'react';
-import playerCardsContext from 'context/playerCardsContext';
-import HealthCounter from './HealthCounter';
-import data from 'data';
+import React, { useContext } from "react";
+import playerCardsContext from "context/playerCardsContext";
+import HealthCounter from "./HealthCounter";
 
 export default function Card(props) {
   const { playerCards } = useContext(playerCardsContext);
 
   const playerHeroId = props.playerHeroId;
   const playerNum = props.playerNum;
-  
+
   // Get card attributes from relevant player
-  const {id, name, health, shieldValue} = playerCards[`player${playerNum}cards`].cards[playerHeroId];
-  
+  const { id, name, health, shieldValue } = playerCards[
+    `player${playerNum}cards`
+  ].cards[playerHeroId];
+
   //TODO: make cardfocused functions - context?
-  // TODO: fix img src issue - should be require(image).default
   return (
     <div id={`${name}-card`} className="card">
       <HealthCounter health={health} />
-      <img src={require(`assets/heroes/${id}.png`).default} className="cardimg" alt={`${name} Card`} />
+      <img
+        src={require(`assets/heroes/${id}.png`).default}
+        className="cardimg"
+        alt={`${name} Card`}
+      />
     </div>
   );
 }
--- src/components/layout/PlayerHand.js
+++ src/components/layout/PlayerHand.js
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import Card from 'components/cards/Card';
-import data from 'data';
-import {Droppable, Draggable} from 'react-beautiful-dnd';
-import rowsContext from 'context/rowsContext';
-import playerCardsContext from 'context/playerCardsContext';
+import React, { useContext } from "react";
+import Card from "components/cards/Card";
+import data from "data";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+import rowsContext from "context/rowsContext";
+import playerCardsContext from "context/playerCardsContext";
 
 export default function PlayerHand(props) {
   // Context
@@ -14,86 +14,97 @@ export default function PlayerHand(props) {
   const playerHandId = `player${props.playerNum}hand`;
   const playerCardsId = `player${props.playerNum}cards`;
   const handCards = rowsState[playerHandId].cardIds;
-  
-  // returns random number between min (inc) and max (exc)
+
+  // Helper function - returns random number between min (inc) and max (exc)
   function getRandInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  // Creates a card with its own health and id unique to the playerCard, returns unique ID
+  // Creates a card with its own health and id unique to the playerCard, returns player-specific ID
   function createPlayerCard(playerNum, heroId) {
-    
+    // Get card values
     const { id, health } = data.heroes[heroId];
-    
-    // Create values unique to that card not stored in data
     const playerHeroId = `${playerNum}${heroId}`;
     const shieldValue = 0;
     const isDiscarded = false;
 
     // Combine values into one new hero object and assign to relevant player
-    const newCard = {playerHeroId, id, health, shieldValue, isDiscarded};
-    
+    const newCard = { playerHeroId, id, health, shieldValue, isDiscarded };
     setPlayerCards({
       ...playerCards,
       [playerCardsId]: {
         ...playerCards[playerCardsId],
         cards: {
           ...playerCards[playerCardsId].cards,
-          [playerHeroId]: {...newCard}
-        }
-      }
-    })
-
+          [playerHeroId]: { ...newCard },
+        },
+      },
+    });
+    // return player-specific id to be used elsewhere
     return playerHeroId;
   }
 
-  // TODO: randomly add a specificed number of cards from heroes to hand
-  // TODO: check if card already drawn/discarded by that player
+  // Draws one random card and puts the card into the player's hand
   function drawCards() {
+    // TODO: specify number of cards to draw?
     // Draw a random card id, then check if it was already drawn, if so draw again
     do {
       const randInt = getRandInt(0, Object.keys(data.heroes).length);
       const randKey = Object.keys(data.heroes)[randInt];
       const newCardId = data.heroes[randKey].id;
-      // Create the player-specific card using the random id 
+      // Create the player-specific card using the random id and get player-specific id
       var playerHeroId = createPlayerCard(props.playerNum, newCardId);
-    }
-    while (playerHeroId in playerCards[playerCardsId].cards);
-    
+    } while (playerHeroId in playerCards[playerCardsId].cards);
 
+    // Create updated array and update state
     const newCardIds = [...rowsState[playerHandId].cardIds, playerHeroId];
     setRowsState({
       ...rowsState,
       [playerHandId]: {
         ...rowsState[playerHandId],
-        cardIds: newCardIds
-      }
+        cardIds: newCardIds,
+      },
     });
   }
 
-  return(
+  return (
     <div className="playerhand">
-    
-      <Droppable droppableId={`player${props.playerNum}hand`} direction="horizontal">
-      {(provided) => (
-        <ul className="handlist" {...provided.droppableProps} ref={provided.innerRef}>
-          {handCards && handCards.map((cardId, index) => {
-            return(
-              <Draggable key={cardId} draggableId={cardId} index={index}>
-                {(provided) => (
-                  <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                    <Card playerHeroId={cardId} playerNum={props.playerNum} />
-                  </li>
-                )}
-              </Draggable>
-            );
-          })}
-          {provided.placeholder}
-        </ul>
-      )}
-      
+      <Droppable
+        droppableId={`player${props.playerNum}hand`}
+        direction="horizontal"
+      >
+        {(provided) => (
+          <ul
+            className="handlist"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {handCards &&
+              handCards.map((cardId, index) => {
+                return (
+                  <Draggable key={cardId} draggableId={cardId} index={index}>
+                    {(provided) => (
+                      <li
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        <Card
+                          playerHeroId={cardId}
+                          playerNum={props.playerNum}
+                        />
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+            {provided.placeholder}
+          </ul>
+        )}
       </Droppable>
-    <button style={{width: '50px', height: '50px'}} onClick={drawCards}>Draw</button>
+      <button style={{ width: "50px", height: "50px" }} onClick={drawCards}>
+        Draw
+      </button>
     </div>
-  ); 
+  );
 }
