b44564d0109c0e330877efdb0c0c27f2659a4b7c
nathanhmiles <60706094+nathanhmiles@users.noreply.github.com>
2021-01-22 17:24:23 +0000

    Move all ability related functions to HeroAbilities from CardFocus

--- src/components/cards/CardFocus.js
+++ src/components/cards/CardFocus.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import HeroAbilities from 'components/cards/HeroAbilities';
 import gameContext from "context/gameContext";
 import update from 'immutability-helper';
 import HealthCounter from "./HealthCounter";
@@ -28,87 +29,7 @@ export default function CardFocus(props) {
     allyEffects,
     isDiscarded,
   } = gameState.playerCards[`player${playerNum}cards`].cards[playerHeroId];
-
-  // Applies the ability result to state
-  function setAbilityResult(abilityResult) {
-    if (abilityResult.type === "row") {
-      // Apply abilities that affect a whole row
-
-      const newState = update(gameState, {
-        rows: {[abilityResult.rowId]: {
-          [abilityResult.rowKey]: {$push: [abilityResult.rowValue]}
-        }}});
-
-      setGameState(newState);
-
-    } else if (abilityResult.type === "card") {
-      // Apply abilities that affect a specific card
-      const targetCardId = abilityResult.targetCardId;
-      const targetPlayer = targetCardId[0];
-      const targetRow = abilityResult.targetRow;
-      
-      const newState = update(gameState, {
-        playerCards: {[`player${targetPlayer}cards`]: {
-          cards: {[targetCardId] :{[abilityResult.cardKey]: {$set: abilityResult.cardValue}}}
-        }}
-      });
-
-      setGameState(newState);
-
-    } else console.log(abilityResult);
-  }
-
-  function setRowSynergy(rowId, synergyCost) {
-    setGameState((prevState) => ({
-      ...prevState,
-      rows: {
-        ...prevState.rows,
-        [rowId]: {
-          ...prevState.rows[rowId],
-          synergy: prevState.rows[rowId].synergy - synergyCost,
-        },
-      },
-    }));
-  }
-
-  // Hero ability functions
-  async function activateAbility1(e) {
-    e.stopPropagation();
-
-    // Check that the card is not in the player's hand
-    if (rowId[0] !== "p") {
-      try {
-
-        unsetCardFocus();
-        
-        // Call the relevant hero's ability, then set state using the result
-        const abilityResult = await ability1();
-        setAbilityResult(abilityResult);
-      } catch (err) {
-        alert(err);
-      }
-    } else alert("Play cards before using abilities!");
-  }
-
-  async function activateAbility2(e) {
-    e.stopPropagation();
-
-    // Check that the card is not in the player's hand
-    if (rowId[0] !== "p") {
-      try {
-        const rowSynergy = gameState.rows[rowId].synergy;
-        unsetCardFocus();
-
-        // Call the relevant hero's ability, then set state using the result, and deduct synergy
-        const abilityResult = await ability2(rowSynergy);
-        setAbilityResult(abilityResult);
-        setRowSynergy(rowId, abilityResult.synergyCost);
-      } catch (err) {
-        setCardFocus(cardFocus);
-        alert(err);
-      }
-    } else alert("Play cards before using abilities!");
-  }
+  
 
   return (
     <div id="cardfocuscontainer">
@@ -123,16 +44,14 @@ export default function CardFocus(props) {
           className="cardimg"
           alt={"Card Focus"}
         />
-        <div
-          id={`${id}-ability1`}
-          className="ability ability1"
-          onClick={activateAbility1}
-        ></div>
-        <div
-          id={`${id}-ability2`}
-          className="ability ability2"
-          onClick={activateAbility2}
-        ></div>
+        <HeroAbilities 
+          playerNum={playerNum}
+          playerHeroId={playerHeroId}
+          rowId={rowId}
+          cardFocus={cardFocus}
+          setCardFocus={setCardFocus}
+          unsetCardFocus={unsetCardFocus}
+        />
       </div>
     </div>
   );
--- src/components/cards/HeroAbilities.js
+++ src/components/cards/HeroAbilities.js
@@ -14,6 +14,51 @@ export default function HeroAbilities(props) {
   const heroId = playerHeroId.slice(1, playerHeroId.length);
   const playerCardsId = `player${playerNum}cards`;
   const rowId = props.rowId;
+  const cardFocus = props.cardFocus;
+  const setCardFocus = props.setCardFocus;
+  const unsetCardFocus = props.unsetCardFocus;
+
+  // Applies the ability result to state
+  function setAbilityResult(abilityResult) {
+    if (abilityResult.type === "row") {
+      // Apply abilities that affect a whole row
+
+      const newState = update(gameState, {
+        rows: {[abilityResult.rowId]: {
+          [abilityResult.rowKey]: {$push: [abilityResult.rowValue]}
+        }}});
+
+      setGameState(newState);
+
+    } else if (abilityResult.type === "card") {
+      // Apply abilities that affect a specific card
+      const targetCardId = abilityResult.targetCardId;
+      const targetPlayer = targetCardId[0];
+      const targetRow = abilityResult.targetRow;
+      
+      const newState = update(gameState, {
+        playerCards: {[`player${targetPlayer}cards`]: {
+          cards: {[targetCardId] :{[abilityResult.cardKey]: {$set: abilityResult.cardValue}}}
+        }}
+      });
+
+      setGameState(newState);
+
+    } else console.log(abilityResult);
+  }
+
+  function setRowSynergy(rowId, synergyCost) {
+    setGameState((prevState) => ({
+      ...prevState,
+      rows: {
+        ...prevState.rows,
+        [rowId]: {
+          ...prevState.rows[rowId],
+          synergy: prevState.rows[rowId].synergy - synergyCost,
+        },
+      },
+    }));
+  }
 
   // Abilities data
   const abilities = {
@@ -146,12 +191,51 @@ export default function HeroAbilities(props) {
       }); 
   }
 
+  // Hero ability functions
+  async function activateAbility1(e) {
+    e.stopPropagation();
+
+    // Check that the card is not in the player's hand
+    if (rowId[0] !== "p") {
+      try {
+
+        unsetCardFocus();
+        
+        // Call the relevant hero's ability, then set state using the result
+        const abilityResult = await ability1();
+        setAbilityResult(abilityResult);
+      } catch (err) {
+        alert(err);
+      }
+    } else alert("Play cards before using abilities!");
+  }
+
+  async function activateAbility2(e) {
+    e.stopPropagation();
+
+    // Check that the card is not in the player's hand
+    if (rowId[0] !== "p") {
+      try {
+        const rowSynergy = gameState.rows[rowId].synergy;
+        unsetCardFocus();
+
+        // Call the relevant hero's ability, then set state using the result, and deduct synergy
+        const abilityResult = await ability2(rowSynergy);
+        setAbilityResult(abilityResult);
+        setRowSynergy(rowId, abilityResult.synergyCost);
+      } catch (err) {
+        setCardFocus(cardFocus);
+        alert(err);
+      }
+    } else alert("Play cards before using abilities!");
+  }
+
   return(
     <div id="abilitiescontainer">
-      <div id="ability1" onClick={() => {abilities[heroId].ability1()}}>
+      <div id="ability1" className="ability ability1" onClick={activateAbility1}>
 
       </div>
-      <div id="ability2" onClick={() => {abilities[heroId].ability2()}}>
+      <div id="ability2" className="ability ability2" onClick={activateAbility2}>
 
       </div>
     </div>
