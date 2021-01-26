ce6b2d9ea9e7454c58ad03a5a643a7e15d47c3ba
nathanhmiles <nathanhmiles@gmail.com>
2021-01-26 18:54:34 +0000

    Improved mobile responsiveness

--- src/components/layout/SynergyCounter.js
+++ src/components/layout/SynergyCounter.js
@@ -2,8 +2,10 @@
 export default function SynergyCounter(props) {
 
   return (
-    <div className="synergycounter counter">
-      <span id={props.id}><h4>{props.synergy}</h4></span>
+    <div className="synergycontainer">
+      <div className="synergycounter counter">
+        <span id={props.id}><h4>{props.synergy}</h4></span>
+      </div>
     </div>
   )
 }
