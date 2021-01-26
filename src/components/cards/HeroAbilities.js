cc0c346d42c1b5fac875cc1f631af809e0b121ab
nathanhmiles <nathanhmiles@gmail.com>
2021-01-26 20:15:24 +0000

    Implemented pharah's abilities

--- src/components/cards/HeroAbilities.js
+++ src/components/cards/HeroAbilities.js
@@ -33,6 +33,24 @@ export default function HeroAbilities(props) {
     }));
   }
 
+  // Applies damage to either shields or health as needed, returning both the shield and health value
+  function applyDamage(damageValue, targetId) {
+    const targetPlayerNum = targetId[0];
+
+    let targetHealth = gameState.playerCards[`player${targetPlayerNum}cards`].cards[targetId].health;
+    let targetShield = gameState.playerCards[`player${targetPlayerNum}cards`].cards[targetId].shield;
+    
+    for (let i = 0; i < damageValue; i++) {
+      if (targetShield > 0) {
+        targetShield -= 1;
+      } else {
+        targetHealth -= 1;
+      }
+    }
+
+    return {targetHealth, targetShield};
+  }
+
   // Abilities data
   const abilities = {
     dva: {
@@ -53,7 +71,6 @@ export default function HeroAbilities(props) {
     },
     dvameka: {
       ability1: {
-        maxTargets: 1,
         run() {
           const newShield = 2;
 
@@ -87,17 +104,8 @@ export default function HeroAbilities(props) {
           
           // Damage own player cards
           for (let cardId of playerRowCardIds) {
-            let targetHealth = gameState.playerCards[`player${playerNum}cards`].cards[cardId].health;
-            let targetShield = gameState.playerCards[`player${playerNum}cards`].cards[cardId].shield;
+            const { targetHealth, targetShield } = applyDamage(damageValue, cardId);
             
-            for (let i = 0; i < damageValue; i++) {
-              if (targetShield > 0) {
-                targetShield -= 1;
-              } else {
-                targetHealth -= 1;
-              }
-            }
-
             setGameState((prevState) => ({
               ...prevState,
               playerCards: {
@@ -116,20 +124,12 @@ export default function HeroAbilities(props) {
               },
             }));
           }
+          
 
           // Damage enemy cards
           for (let cardId of enemyPlayerRowCardIds) {
-            let targetHealth = gameState.playerCards[`player${enemyPlayer}cards`].cards[cardId].health;
-            let targetShield = gameState.playerCards[`player${enemyPlayer}cards`].cards[cardId].shield;
+            const { targetHealth, targetShield } = applyDamage(damageValue, cardId);  
             
-            for (let i = 0; i < damageValue; i++) {
-              if (targetShield > 0) {
-                targetShield -= 1;
-              } else {
-                targetHealth -= 1;
-              }
-            }
-
             setGameState((prevState) => ({
               ...prevState,
               playerCards: {
@@ -148,9 +148,9 @@ export default function HeroAbilities(props) {
               },
             }));
           }
+            
 
           // After effects
-
           // Create new dva card 
           const newDva = helper.createPlayerCard(playerNum, 'dva');
           // Remove dvameka card from row (still exists in playercards)
@@ -291,9 +291,122 @@ export default function HeroAbilities(props) {
       },
     },
 
+    pharah: {
+      ability1: {
+        run() {
+
+          return new Promise((resolve, reject) => {
+
+            $(".card").on("click", (e) => {
+
+              const targetCardId = $(e.target).closest(".card").attr("id");
+              const enemyPlayer = targetCardId[0];
+              const targetRow = $(e.target).closest(".row").attr("id");
+              
+              $(".card").off("click");
+
+              if (targetRow[0] === "p" || targetRow[0] === playerNum) {
+                // Check target is valid
+                reject("Incorrect target row");
+                return;
+
+              } else if (targetRow[1] !== 'b') {
+                // Move target back a row if not already in last row
+                const newRow = `${enemyPlayer}${targetRow[1] === 'f' ? 'm' : 'b'}`;
+                console.log(newRow);
+                const updatedTargetRowCardIds = gameState.rows[targetRow].cardIds.filter(cardId => cardId !== targetCardId);
+                const newRowCardIds = [...gameState.rows[newRow].cardIds, targetCardId];
+                
+                setGameState((prevState) => ({
+                  ...prevState,
+                  rows: {
+                    ...prevState.rows,
+                    [targetRow]: {
+                      ...prevState.rows[targetRow],
+                      cardIds: updatedTargetRowCardIds,
+                    },
+                    [newRow]: {
+                      ...prevState.rows[newRow],
+                      cardIds: newRowCardIds,
+                    },
+                  },
+                }));
+              }
+
+              // Reduce synergy of target row
+              setGameState((prevState) => ({
+                ...prevState,
+                rows: {
+                  ...prevState.rows,
+                  [targetRow]: {
+                    ...prevState.rows[targetRow],
+                    synergy: (prevState.rows[targetRow].synergy - 2),
+                  },
+                },
+              }));
+
+              resolve();
+            });
+          });
+
+        },
+      },
+      ability2: {
+        maxTargets: 3,
+        synergyCost: 3,
+        run() {
+          
+          return new Promise((resolve, reject) => {
+            
+            $(".card").on("click", (e) => {
+              
+              const targetCardId = $(e.target).closest(".card").attr("id");
+              const enemyPlayer = targetCardId[0];
+              const targetRow = $(e.target).closest(".row").attr("id");
+
+              $(".card").off("click");
+              // Check target is valid
+              if (targetRow[0] === "p" || targetRow[0] === playerNum) {
+                reject("Incorrect target");
+                return;
+              }
+
+              const damageValue = 2;
+
+              const { targetHealth, targetShield } = applyDamage(damageValue, targetCardId);
+
+              // Apply abilities that affect a specific card
+
+              setGameState((prevState) => ({
+                ...prevState,
+                playerCards: {
+                  ...prevState.playerCards,
+                  [`player${enemyPlayer}cards`]: {
+                    ...prevState.playerCards[`player${enemyPlayer}cards`],
+                    cards: {
+                      ...prevState.playerCards[`player${enemyPlayer}cards`]
+                        .cards,
+                      [targetCardId]: {
+                        ...prevState.playerCards[`player${enemyPlayer}cards`]
+                          .cards[targetCardId],
+                        health: targetHealth,
+                        shield: targetShield,
+                      },
+                    },
+                  },
+                },
+              }));
+
+              resolve();
+            });
+          });
+
+        },
+      },
+    },
+
     widowmaker: {
       ability1: {
-        maxTargets: 1,
         run() {
           return new Promise((resolve, reject) => {
             $(".row").on("click", (e) => {
@@ -395,12 +508,14 @@ export default function HeroAbilities(props) {
       try {
         unsetCardFocus();
         
-        // Allow the ability to be triggered more than once if relevant
-        let i = 0;
-        do {
-          await abilities[heroId].ability1.run();
-          i += 1;
-        } while (i < maxTargets);
+        if ('maxTargets' in abilities[heroId].ability1) {
+          // Allow the ability to be triggered more than once if relevant
+          let i = 0;
+          do {
+            await abilities[heroId].ability1.run();
+            i += 1;
+          } while (i < maxTargets);
+        } else await abilities[heroId].ability1.run();
         
       } catch (err) {
         alert(err);
@@ -412,7 +527,7 @@ export default function HeroAbilities(props) {
     e.stopPropagation();
     const synergyCost = abilities[heroId].ability2.synergyCost;
     const rowSynergy = gameState.rows[rowId].synergy;
-
+    
     // Check that the card is not in the player's hand
     if (rowId[0] !== "p") {
       // Check there is sufficient synergy to use the ability
@@ -420,7 +535,16 @@ export default function HeroAbilities(props) {
         // Call the relevant hero's ability and deduct synergy
         try {
           unsetCardFocus();
-          await abilities[heroId].ability2.run();
+          
+          if ('maxTargets' in abilities[heroId].ability2) {
+            const maxTargets = abilities[heroId].ability2.maxTargets;
+            let i = 0;
+            do {
+              await abilities[heroId].ability2.run();
+              i += 1;
+            } while (i < maxTargets);
+          } else await abilities[heroId].ability2.run();
+          
           setRowSynergy(rowId, synergyCost);
         } catch (err) {
           alert(err);
