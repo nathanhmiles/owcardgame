502de5dd70d2311ed83b3be63e34f56f391c7021
Nathan Hubbard-Miles <nathanhmiles@gmail.com>
2026-04-04 15:55:16 +0100

    refactor(player-card.factory): property initialisation

--- src/player-card.factory.ts
+++ src/player-card.factory.ts
@@ -6,22 +6,27 @@ export class PlayerCard {
     public playerNum: 1 | 2;
     public heroId: HeroId;
 
-    public heroData: HeroData = { ...data.heroes[this.heroId] };
+    public heroData: HeroData;
 
-    public maxHealth = this.heroData.health;
-    public playerHeroId = `${this.playerNum}${this.heroId}`;
+    public maxHealth: number;
+    public playerHeroId: string;
     public shield = 0;
     public effects?: HeroEffectData;
     public enemyEffects = [];
     public allyEffects = [];
     public ability1Used = false;
     public ability2Used = false;
-    public isPlayed = this.heroId === 'dva' ? true : false;
+    public isPlayed: boolean;
     public isDiscarded = false;
 
     constructor(playerNum: 1 | 2, heroId: HeroId) {
         this.playerNum = playerNum;
         this.heroId = heroId;
+
+        this.heroData = data.heroes[heroId];
+        this.maxHealth = this.heroData.health;
+        this.playerHeroId = `${this.playerNum}${this.heroId}`;
+        this.isPlayed = this.heroId === 'dva' ? true : false;
 
         // Add hero effects to card, and insert playerHeroId for future use
         if ('effects' in this.heroData) {
